734d1c5ee15583362d942c9ba07580a44c6c2965
vinicius-coimbra <vctellesfigueiredo@gmail.com>
2025-05-05 23:33:39 -0300

    (6/5/25) eu fiz o indioma do sistema mas achei paia

--- indioma.java
+++ indioma.java
@@ -1,11 +1,8 @@
-import java.util.Locale;
-
-import javax.print.DocFlavor.STRING;
-
+import java.util.*;
 public class indioma {
-    public static void main(String[] args){
-
-        Locale indioma = Locale.forLanguageTag()
+    public static void main(String[] args)  {
+        Locale idioma = Locale.getDefault();
+        System.out.println("linguagem:" + idioma.getDisplayLanguage());
 
     }
     
